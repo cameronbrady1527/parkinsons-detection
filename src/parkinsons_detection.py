de60e0687d80d820bd27e7d249843a1d2d653852
Cameron <cameronbrady1527@gmail.com>
2024-10-15 18:03:24 -0400

    Project setup

---
+++ src/parkinsons_detection.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# Load the Dataset:
+data = pd.read_csv('data/parkinsons.data')
+
